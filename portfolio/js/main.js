344d7259085adf8c6ffcd6ca68a7a2b77ad9eb56
emilysu00 <9210085256emily@gmail.com>
2024-01-29 15:13:36 +0800

    Update main.js

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://unpkg.com/three@0.160.1/build/three.module.js";
+import * as THREE from "https://cdn.skypack.dev/three@0.160.1";
 import { RGBELoader } from "https://unpkg.com/three@0.160.1/examples/jsm/loaders/RGBELoader.js";
 import { OBJLoader } from "https://unpkg.com/three@0.160.1/examples/jsm/loaders/OBJLoader.js";
 
