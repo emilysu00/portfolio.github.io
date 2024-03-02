8e5b997b6f4aa4ea03438951a832940f1d4fbc2c
emilysu00 <9210085256emily@gmail.com>
2024-03-02 22:51:28 +0800

    3/2

--- portfolio/js/about.js
+++ portfolio/js/about.js
@@ -168,28 +168,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   requestAnimationFrame(animate);
 });
-
-//hamburgur
-let bars = document.querySelector(".clickable-bar");
-let bar_one = document.querySelector(".bar-1");
-let bar_two = document.querySelector(".bar-2");
-let bar_three = document.querySelector(".bar-3");
-bars.addEventListener("click", function () {
-  if (bars.dataset.clicked === "0") {
-    document.getElementsByClassName("frame")[0].classList.add("active");
-    document.getElementsByClassName("menu-item")[0].classList.add("active");
-    bar_two.style.visibility = "hidden";
-    bar_one.style.transform = "rotate(-45deg)";
-    bar_one.style.marginTop = "3vh";
-    bar_three.style.transform = "rotate(45deg)";
-    bar_three.style.marginTop = "-1.1vh";
-    bars.dataset.clicked = "1";
-  } else {
-    document.getElementsByClassName("frame")[0].classList.remove("active");
-    document.getElementsByClassName("menu-item")[0].classList.remove("active");
-    bar_one.removeAttribute("style");
-    bar_three.removeAttribute("style");
-    bar_two.style.visibility = "visible";
-    bars.dataset.clicked = "0";
-  }
-});
